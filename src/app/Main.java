7dc0e606ee06702d80051ec6eae01e8b0b5b27b1
DenisLohvin <l.denis.r.work@gmail.com>
2022-11-11 19:30:29 +0200

    Removing an incorrect variable declaration

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,8 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
+
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
