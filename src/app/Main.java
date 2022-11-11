8a58b1aec454ea6b6d3a95cf46b21cf2e0b2c381
DenisLohvin <l.denis.r.work@gmail.com>
2022-11-11 19:32:04 +0200

    Declaration of variable "a"

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,7 @@
 package app;
 
 public class Main {
-
+ static int a;
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
